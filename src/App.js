543a15bc66baf3806dc13d68a36e93cac427c055
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-28 00:56:26 +0200

    New comps added to App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 import useFetchJobs from './useFetchJobs';
 import { Container } from 'react-bootstrap';
+import Job from './Job';
+import Loader from './Loader';
+import JobsPagination from './JobsPagination';
 
 const App = () => {
-  const { jobs, loading, error } = useFetchJobs();
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
+  let { jobs, loading, error } = useFetchJobs();
 
   return (
-    <Container>
-      {loading && <h1>Loading...</h1>}
+    <Container className='my-5'>
+      <h1>GitHub Jobs</h1>
+      <JobsPagination page={page} setPage={setPage} />
+      {loading && <Loader />}
       {error && <h1>Error. Try Refreshing...</h1>}
-      {!loading && !error && <h1>{jobs.length}</h1>}
+      {!loading && !error && jobs.map((job) => <Job key={job.id} job={job} />)}
+      <JobsPagination page={page} setPage={setPage} />
     </Container>
   );
 };
