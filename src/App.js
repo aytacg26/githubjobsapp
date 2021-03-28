8b12ab4afc66e054c13720562232f020d331a0f4
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-28 13:10:39 +0300

    Search and Pagination Added and completed

--- src/App.js
+++ src/App.js
@@ -5,20 +5,43 @@ import { Container } from 'react-bootstrap';
 import Job from './Job';
 import Loader from './Loader';
 import JobsPagination from './JobsPagination';
+import SearchForm from './SearchForm';
 
 const App = () => {
   const [params, setParams] = useState({});
   const [page, setPage] = useState(1);
-  let { jobs, loading, error } = useFetchJobs();
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
+
+  const handleParamChange = (e) => {
+    const param = e.target.name;
+    const value = e.target.value;
+    setPage(1);
+    setParams((prevParams) => {
+      return { ...prevParams, [param]: value };
+    });
+  };
 
   return (
     <Container className='my-5'>
       <h1>GitHub Jobs</h1>
-      <JobsPagination page={page} setPage={setPage} />
+      <SearchForm params={params} onParamChange={handleParamChange} />
+      {!loading && (
+        <JobsPagination
+          page={page}
+          setPage={setPage}
+          hasNextPage={hasNextPage}
+        />
+      )}
       {loading && <Loader />}
       {error && <h1>Error. Try Refreshing...</h1>}
       {!loading && !error && jobs.map((job) => <Job key={job.id} job={job} />)}
-      <JobsPagination page={page} setPage={setPage} />
+      {!loading && (
+        <JobsPagination
+          page={page}
+          setPage={setPage}
+          hasNextPage={hasNextPage}
+        />
+      )}
     </Container>
   );
 };
